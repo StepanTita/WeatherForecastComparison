e03f6ab9dadf6afb1290aadf3b3b39197b889a32
StepanTita <stapan.tytarenko@nure.ua>
2019-07-05 19:04:04 +0300

    util (console) added

---
+++ src/util/average_util.py
@@ -0,0 +1,147 @@
+import sys
+sys.path.append('../')
+
+import pandas as pd
+import numpy as np
+import model.load_data as m_ld
+import math
+import plotly
+import plotly.plotly as py
+from plotly import tools
+import plotly.graph_objs as go
+import statistics as st
+
+def get_biases(pred, real):
+	return [pr - rl for pr, rl in zip(pred, real)]
+
+def read_data(path="../../data/weatherHistory.csv"):
+	df = pd.read_csv(path)
+	temp_df = df.drop(axis=1, labels=['Summary', 'Humidity', 'Wind Speed (km/h)', 'Wind Bearing (degrees)', 'Visibility (km)', 'Loud Cover', 'Pressure (millibars)', 'Daily Summary', 'Precip Type'])
+	temp_df = temp_df.rename(columns={"Temperature (C)": "real", "Apparent Temperature (C)": "predicted"})#'Formatted Date', 
+	return temp_df
+
+def draw_biases(data, biases):
+	# Create a trace
+	avg = st.mean(biases)
+	trace1 = go.Scatter(
+	    x = [i for i in range(len(biases))],
+	    y = biases,
+	    name="Bias"
+	)
+	trace2 = go.Scatter(
+		x = [i for i in range(len(biases))],
+		y = [avg for __ in range(len(biases))],
+		name="Average bias"
+	)
+	# trace3 = go.Scatter(
+	# 	x = [i for i in range(len(biases))],
+	# 	y = [data['real'][i] for i in range(len(biases))],
+	# 	name="Real Temperature",
+	# 	mode="markers+text",
+	# 	text="y"
+	# )
+	trace4 = go.Scatter(
+		x = [i for i in range(len(biases))],
+		y = [data['real'][i] for i in range(len(biases))],
+		name="Real Temperature"
+	)
+	trace5 = go.Scatter(
+		x = [i for i in range(len(biases))],
+		y = [data['predicted'][i] for i in range(len(biases))],
+		name="Predicted Temperature"
+	)
+	# trace6 = go.Scatter(
+	# 	x = [i for i in range(len(biases))],
+	# 	y = [data['predicted'][i] for i in range(len(biases))],
+	# 	name="Real Temperature",
+	# 	mode="markers+text",
+	# 	text="y"
+	# )
+
+	#data = [trace1, trace2]
+	# layout = dict(title = 'Bias and average bias Values',
+ #              yaxis = dict(title = 'Bias'),
+ #              xaxis=dict(
+ #              		title = 'Date',
+ #                  	rangeselector=dict(
+ #                      	buttons=list([
+ #                          	dict(count=1,
+ #                               	label='1m',
+ #                               	step='month',
+ #                               	stepmode='backward'),
+ #                          	dict(count=6,
+ #                               	label='6m',
+ #                               	step='month',
+ #                               	stepmode='backward'),
+ #                          	dict(count=1,
+ #                              	label='YTD',
+ #                             	step='year',
+ #                              	stepmode='todate'),
+ #                          	dict(count=1,
+ #                              	label='1y',
+ #                              	step='year',
+ #                              	stepmode='backward'),
+ #                          	dict(step='all')
+ #                      	])
+ #                  	),
+ #                  	rangeslider=dict(
+ #                      	visible = True
+ #                  	),
+ #                  	type='date'
+ #              	)
+ #            )
+	#fig = go.Figure(data=data, layout=layout)
+
+	fig = tools.make_subplots(rows=2, cols=1)
+	fig.append_trace(trace1, 1, 1)
+	fig.append_trace(trace2, 1, 1)
+	fig.append_trace(trace4, 2, 1)
+	fig.append_trace(trace5, 2, 1)
+
+	#fig = dict(data=data, layout=layout)
+	fig['layout'].update(title = 'Bias and average bias Values',
+              yaxis = dict(title = 'Bias'),
+              xaxis=dict(
+              		title = 'Date',
+                  	rangeselector=dict(
+                      	buttons=list([
+                          	dict(count=1,
+                               	label='1m',
+                               	step='month',
+                               	stepmode='backward'),
+                          	dict(count=6,
+                               	label='6m',
+                               	step='month',
+                               	stepmode='backward'),
+                          	dict(count=1,
+                              	label='YTD',
+                             	step='year',
+                              	stepmode='todate'),
+                          	dict(count=1,
+                              	label='1y',
+                              	step='year',
+                              	stepmode='backward'),
+                          	dict(step='all')
+                      	])
+                  	),
+                  	rangeslider=dict(
+                      	visible = True
+                  	),
+                  	type='date'
+              	))
+	
+	plotly.offline.plot(fig, filename='../../graphs/bias-graph')
+
+def main():
+	THRESH = 5
+	data = read_data()
+	biases = get_biases(data['predicted'], data['real'])
+	draw_biases(data, biases[9000:10000])
+	for thr in range(1, THRESH + 1):
+		result = [0 if abs(bias) >= thr else 1 for bias in biases]
+		print("Thresh value : " + str(thr))
+		print("Accuracy:", sum(result) / len(result))
+	
+
+if __name__ == '__main__':
+	main()
---
+++ src/util/fitch_util.py
@@ -0,0 +1,107 @@
+import sys
+sys.path.append('../')
+
+import pandas as pd
+import numpy as np
+import model.load_data as m_ld
+import math
+
+def fitch_tree(data):
+	tree = [[] for __ in range(2 * int(math.log2(len(data['sources'])) + 1))]
+	tree[0] += [src for src in data['sources']]
+
+	prev = 0
+	for step in range(1, len(tree)):
+		if len(tree[prev]) % 2 != 0:
+			tree[prev].append({
+					"name" : ["pseudo"],
+					"acc_t" : {"src" : "pseudo", "val" : 0}, 
+					"acc_h" : {"src" : "pseudo", "val" : 0}, 
+					"acc_w" : {"src" : "pseudo", "val" : 0}
+				})
+		
+		for i in range(0, len(tree[prev]), 2):
+			last = len(tree[step])
+			tree[step].append({
+				"name" : [],
+				"acc_t" : {"src" : "pseudo", "val" : 0}, 
+				"acc_h" : {"src" : "pseudo", "val" : 0}, 
+				"acc_w" : {"src" : "pseudo", "val" : 0}
+			})
+			# -----1-----
+			if tree[prev][i]["acc_t"]["val"] > tree[prev][i + 1]["acc_t"]["val"]:
+				new_name = tree[prev][i]["acc_t"]["src"]
+				new_val = tree[prev][i]["acc_t"]["val"]
+				if new_name not in tree[step][last]["name"]:
+					tree[step][last]["name"].append(new_name)
+				tree[step][last]["acc_t"]["src"] = new_name
+				tree[step][last]["acc_t"]["val"] = new_val
+			else:
+				new_name = tree[prev][i + 1]["acc_t"]["src"]
+				new_val = tree[prev][i + 1]["acc_t"]["val"]
+				if new_name not in tree[step][last]["name"]:
+					tree[step][last]["name"].append(new_name)
+				tree[step][last]["acc_t"]["src"] =  new_name
+				tree[step][last]["acc_t"]["val"] = new_val
+			# -----2-----
+			if tree[prev][i]["acc_h"]["val"] > tree[prev][i + 1]["acc_h"]["val"]:
+				new_name = tree[prev][i]["acc_h"]["src"]
+				new_val = tree[prev][i]["acc_h"]["val"]
+				if new_name not in tree[step][last]["name"]:
+					tree[step][last]["name"].append(new_name)
+				tree[step][last]["acc_h"]["src"] =  new_name
+				tree[step][last]["acc_h"]["val"] = new_val
+			else:
+				new_name = tree[prev][i + 1]["acc_h"]["src"]
+				new_val = tree[prev][i + 1]["acc_h"]["val"]
+				if new_name not in tree[step][last]["name"]:
+					tree[step][last]["name"].append(new_name)
+				tree[step][last]["acc_h"]["src"] =  new_name
+				tree[step][last]["acc_h"]["val"] = new_val
+			# ------3------
+			if tree[prev][i]["acc_w"]["val"] > tree[prev][i + 1]["acc_w"]["val"]:
+				new_name = tree[prev][i]["acc_w"]["src"]
+				new_val = tree[prev][i]["acc_w"]["val"]
+				if new_name not in tree[step][last]["name"]:
+					tree[step][last]["name"].append(new_name)
+				tree[step][last]["acc_w"]["src"] =  new_name
+				tree[step][last]["acc_w"]["val"] = new_val
+			else:
+				new_name = tree[prev][i + 1]["acc_w"]["src"]
+				new_val = tree[prev][i + 1]["acc_w"]["val"]
+				if new_name not in tree[step][last]["name"]:
+					tree[step][last]["name"].append(new_name)
+				tree[step][last]["acc_w"]["src"] =  new_name
+				tree[step][last]["acc_w"]["val"] = new_val
+			
+		if len(tree[step]) == 1:
+			break
+		prev = step
+	return tree
+
+def my_print(tree):
+	flag = False
+	for i in range(len(tree)):
+		for j in range(len(tree[i])):
+			print(tree[i][j]["name"], end=" ")
+			if (len(tree[i]) == 1):
+				flag = True
+				print("\nResult: ")
+				print(">> Temperature accuracy(" + tree[i][j]["acc_t"]["src"] + "): " + str(tree[i][j]["acc_t"]["val"]))
+				print(">> Humidity accuracy(" + tree[i][j]["acc_h"]["src"] + "): " + str(tree[i][j]["acc_h"]["val"]))
+				print(">> Wind speed accuracy(" + tree[i][j]["acc_w"]["src"] + "): " + str(tree[i][j]["acc_w"]["val"]), end="")
+		print("\n------------------------")
+		if flag:
+			break
+		
+
+
+def test_fitch():
+	data = m_ld.read_data("../../data/sources.json")
+	tree = fitch_tree(data)
+	#print_tree(tree)
+	my_print(tree)
+
+if __name__ == '__main__':
+	#main()
+	test_fitch()
